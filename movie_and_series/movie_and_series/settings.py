a6857431b636ae02c39e629c482237e591e71dce
Sebastian Molina <sebaasmh2002@hotmail.com>
2023-05-06 18:51:04 -0500

    add db

--- movie_and_series/movie_and_series/settings.py
+++ movie_and_series/movie_and_series/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path, os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,8 +92,12 @@ WSGI_APPLICATION = 'movie_and_series.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('PGDATABASE', cast=str),
+        'USER': config('PGUSER', cast=str),
+        'PASSWORD': config('PGPASSWORD', cast=str),
+        'HOST': config('DATABASE_URL', cast=str),
+        'PORT': '7745',
     }
 }
 
